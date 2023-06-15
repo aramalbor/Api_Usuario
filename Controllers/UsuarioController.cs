b6b42c4178bc9470db66d452facdb5d649bde792
XCH <123770222+Xchel-A@users.noreply.github.com>
2023-06-15 14:42:32 -0600

    Auth

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -3,28 +3,37 @@ using Api_Usuario.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace Api_Usuario.Controllers
 {
-    [Authorize]
+    
     [Route("api/[controller]")]
     [ApiController]
     public class UsuarioController : Controller
     {
 
+   
+        private readonly IConfiguration _configuration;
         private readonly UsuarioContext _context;
 
-        public UsuarioController(UsuarioContext context)
+        public UsuarioController(UsuarioContext context, IConfiguration configuration)
         {
+            _configuration = configuration;
             _context = context;
         }
+        [Authorize]
         [HttpGet]
         public IEnumerable<Usuario> AllUsers() 
         {
             return _context.Usuarios.OrderBy(x => x.Id);    
         }
-
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<Usuario>> GetUsuario(int id)
         {
@@ -41,7 +50,7 @@ namespace Api_Usuario.Controllers
 
             return usuarios;
         }
-
+        [Authorize]
         [HttpGet("clave")]
         public async Task<ActionResult<string>> UsuariosByClave(string clave)
         {
@@ -58,7 +67,7 @@ namespace Api_Usuario.Controllers
 
             return usuarios.Region;
         }
-
+        [Authorize]
         [HttpPost]
         [Route("PostUsuarios")]
         public async Task<ActionResult<Usuario>> PostUsuarios(Usuario usuarios)
@@ -82,5 +91,70 @@ namespace Api_Usuario.Controllers
 
             return CreatedAtAction("GetUsuario", new { id = usuarios.Id }, usuarios);
         }
-    } 
+
+        [HttpPost]
+        [Route("Auth")]
+        public async Task<ActionResult> Auth(string clave)
+        {
+            if (_context.Usuarios == null)
+            {
+                return Problem("Entity set 'UsuariosContext.Usuarios'  is null.");
+            }
+            var user = _context.Usuarios.Where(c => c.Uid == clave);
+            if(user.Count()== 1)
+            {
+                var token = GenerateToken(user.First().Uid);
+                return Ok(new { Usuario = user, Token = token });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+
+           
+        }
+        private string GenerateToken(string uid)
+        {
+            var secretKey = _configuration.GetValue<string>("JwtSettings:SecretKey");
+            var issuer = _configuration.GetValue<string>("JwtSettings:Issuer");
+            var audience = _configuration.GetValue<string>("JwtSettings:Audience");
+            var expirationInMinutes = _configuration.GetValue<int>("JwtSettings:ExpirationInMinutes");
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, uid),
+                new Claim(JwtRegisteredClaimNames.Sub, uid),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMonths(8),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        [Authorize]
+        [HttpGet("getSession")]
+        public IActionResult Protected()
+        {
+            // Este endpoint está protegido por la autenticación JWT
+            // Solo los usuarios autenticados pueden acceder aquí
+            var username = User.Identity?.Name;
+
+            var dataSession = getUser(username);
+
+            return Ok(dataSession);
+        }
+
+        private object getUser(string username)
+        {
+            return _context.Usuarios.Where(c => c.Uid == username);
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,33 +1,89 @@
 using Microsoft.EntityFrameworkCore;
 using Api_Usuario.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddJsonFile("appsettings.json");
+
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<UsuarioContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// JWT Bearer configuration
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSettings.GetValue<string>("SecretKey");
+var issuer = jwtSettings.GetValue<string>("Issuer");
+var audience = jwtSettings.GetValue<string>("Audience");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = issuer,
+            ValidAudience = audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+        };
+    });
+
+// Swagger configuration
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
+app.UseAuthentication();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
